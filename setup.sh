29ae3a1f59cc985d9df4e2c3db736f82b7cc4fab
Casey Beach <beachc@gmail.com>
2016-05-21 17:11:40 -0700

    Add programming languages

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash 
 
-pip install paramiko PyYAML Jinja2 httplib2 six nose passlib pycrypto
+apt-get update
+apt-get upgrade
+apt-get install git-core python python-pip
+pip install --upgrade pip
+pip install paramiko PyYAML Jinja2 httplib2 six nose passlib pycrypto virtualenv virtualenvwrapper
 git clone git://github.com/ansible/ansible.git --recursive
 
 cd ./ansible
