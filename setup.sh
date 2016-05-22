de6f4555ba21e040bc90596667c7d6221c4c460e
Casey Beach <beachc@gmail.com>
2016-05-22 15:10:22 -0700

    Fix dependency problems

--- setup.sh
+++ setup.sh
@@ -2,12 +2,29 @@
 
 apt-get -y update
 apt-get -y upgrade
-apt-get install -y git-core python python-pip
+apt-get install -y \
+    build-essential \
+    git-core \
+    python \
+    python-pip \
+    libffi-dev \
+    libssl-dev 
+
 pip install --upgrade pip
-pip install paramiko PyYAML Jinja2 httplib2 six nose passlib pycrypto virtualenv virtualenvwrapper
+pip install  \
+    paramiko \
+    PyYAML \
+    Jinja2 \
+    httplib2 \
+    six \
+    nose \
+    passlib \
+    pycrypto \
+    virtualenv \
+    virtualenvwrapper
 
 cd ./ansible
 source ./hacking/env-setup
 
-stow -t ${HOME} dotfiles
 bin/ansible-playbook -K -i , ../provision.yml
+stow -t ${HOME} dotfiles
