dfac7aecdabb98af13da057d3139de7d09f8ce58
Casey Beach <beachc@gmail.com>
2017-12-03 11:55:58 -0800

    Add a few more packages to vim

--- install-vim-packages.sh
+++ install-vim-packages.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
 apt-vim install -y https://github.com/kien/rainbow_parentheses.vim.git
+apt-vim install -y https://github.com/gregsexton/MatchTag.git
+apt-vim install -y https://github.com/Lokaltog/vim-powerline.git
+apt-vim install -y https://github.com/derekwyatt/vim-scala.git
+apt-vim install -y https://github.com/Glench/Vim-Jinja2-Syntax.git
+apt-vim install -y https://github.com/orestis/pysmell.git
+apt-vim install -y https://github.com/klen/python-mode.git
+apt-vim install -y https://github.com/ervandew/supertab.git
+apt-vim install -y https://github.com/majutsushi/tagbar.git
+apt-vim install -y https://github.com/airblade/vim-gitgutter.git
+apt-vim install -y https://github.com/plasticboy/vim-markdown.git
